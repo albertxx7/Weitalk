637f02d17315ac8beb52c37f019e4767112fe5f4
albertxx7 <a979845623@gmail.com>
2022-11-19 18:01:41 +0000

    初始化樣式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,10 +3,45 @@
 
 <head>
     <title>@yield('title', 'weitalk - 聊聊')</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <script src="/js/app.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
-    @yield('content')
+
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <title>@yield('title', 'Weibo App') - Laravel</title>
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+
+    <body>
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="/">Weitalk 微聊</a>
+                <ul class="navbar-nav justify-content-end">
+                    <li class="nav-item"><a class="nav-link" href="/help">幫助</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">登入</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+
+    </html>
+
+
+    <div class="container">
+        @yield('content')
+    </div>
+
 </body>
 
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
-@section('title')
+
 @section('content')
-    <h1>主页</h1>
+    <div class="bg-light p-3 p-sm-5 rounded">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在所看到的是 <a href="">Laravel </a>
+        </p>
+        <p>
+            一切，從現在開始
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">馬上註冊</a>
+        </p>
+    </div>
 @stop
