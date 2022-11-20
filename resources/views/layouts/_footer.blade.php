7a7f425d699cb7f345ddd751a7cae4ce6fdb87f8
albertxx7 <a979845623@gmail.com>
2022-11-20 09:16:30 +0000

    用戶註冊表單建立

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,10 +1,10 @@
 <footer class="footer">
     <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
     <a href="https://learnku.com/laravel/courses" target=_blank class="text-decoration-none">
-        刻意练习，每日精进
+        分享，讓世界更好
     </a>
 
     <div class="float-end">
-        <a href="{{ route('about') }}" class="text-decoration-none">关于</a>
+        <a href="{{ route('about') }}" class="text-decoration-none">關於</a>
     </div>
 </footer>
