033072192eb0e84bb3d05ad078c0746f65c88150
albertxx7 <a979845623@gmail.com>
2022-11-28 11:59:22 +0000

    管理員可删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -72,7 +72,15 @@ class UsersController extends Controller
 
     public function index()
     {
-        $users = User::paginate(1);
+        $users = User::paginate(5);
         return view('users.index', compact('users'));
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
     }
 }
