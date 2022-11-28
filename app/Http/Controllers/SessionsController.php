4407fc9b68bd4a7ea747f52bbbe1ff7b67a3f68f
albertxx7 <a979845623@gmail.com>
2022-11-28 11:09:20 +0000

    登入後記住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -19,18 +19,15 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
 
-        if (Auth::attempt($credentials)) {
-            // 登入後的操作
+        if (Auth::attempt($credentials, $request->has('remember'))) {
             session()->flash('success', '歡迎回來！');
             return redirect()->route('users.show', [Auth::user()]);
         } else {
-            // 登入失敗的操作
-            session()->flash('danger', '很抱歉，信箱跟密碼不匹配');
+            session()->flash('danger', '很抱歉，密碼不符');
             return redirect()->back()->withInput();
         }
-
-        return;
     }
+
 
     public function destroy()
     {
